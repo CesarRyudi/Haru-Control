7e83c6b92a700fefae4cfb68d623abd8259511ac
cesarryudi <cesarryudi@gmail.com>
2026-07-28 17:36:07 -0300

    chore: reverte dockerfile para migrate deploy e salva a nova migration gerada

---
+++ apps/api/prisma/migrations/20260728203544_add_categories/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "products" ADD COLUMN     "category_id" TEXT;
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DECIMAL(10,2),
+    "observation" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
